8cb336868e552a32e2a5083a36d98221d4ea29ed
freevision <freevision@freevisiondeimac.local>
2019-01-10 14:47:56 +0800

    Initial Commit

---
+++ BoxzCurveDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BoxzCurveDemo
+//
+//  Created by freevision on 2019/1/10.
+//  Copyright © 2019年 Boxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BoxzCurveDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BoxzCurveDemo
+//
+//  Created by freevision on 2019/1/10.
+//  Copyright © 2019年 Boxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
